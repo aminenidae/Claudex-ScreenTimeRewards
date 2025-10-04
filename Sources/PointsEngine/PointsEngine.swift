fafc56b9af3f698821514d3aed0852df6e4b6aa7
aminenidae <amine.nidae@icloud.com>
2025-10-04 17:02:32 -0500

    Fix build: drop unnecessary Core import from PointsEngine

--- Sources/PointsEngine/PointsEngine.swift
+++ Sources/PointsEngine/PointsEngine.swift
@@ -1,8 +1,6 @@
 import Foundation
-import Core
 
 public final class PointsEngine {
     public init() {}
     public func accrue(pointsPerMinute: Int, minutes: Int) -> Int { max(0, pointsPerMinute * minutes) }
 }
-
